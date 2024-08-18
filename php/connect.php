bca45812edb2cb982a36905eb258f0d30464d3f7
seduns <168711207+seduns@users.noreply.github.com>
2024-08-18 10:11:06 +0800

    Delete php directory

--- php/connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "healthhub";
-   
-    $conn = mysqli_connect($servername,$username,$password,$database);
-   
-   ?>
